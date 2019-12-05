5355dfccefcf0c9880a6374ff081017dfb887cc3
Ricotan-Naboon <shun812linus@gmail.com>
2019-12-05 14:48:51 +0900

    トークン流出芸人やめてくれ

--- key.py
+++
@@ -1,6 +0,0 @@
-SLACK_BOT_TOKEN = 'xoxp-508287033857-508889631426-860716130320-fec1b2002fc668dbfdfcc0dd97d3bf93'
-#Slackの投稿用
-access_token = 'fdddab7ac8f568671147dbeb67e3847c5ac2f72c'
-#bitlyのトークン
-slackch = '#学習用チャンネル'
-#投稿するチャンネル
