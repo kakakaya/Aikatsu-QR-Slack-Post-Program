30957e233ca92b7824f10d7a7cebc2caf0da259d
Ricotan-Naboon <shun812linus@gmail.com>
2019-12-12 13:58:54 +0900

    URLダウンロード廃止しました

--- read.py
+++ read.py
@@ -7,9 +7,7 @@ import requests
 import json
 import pprint
 import time
-import urllib
 import sys
-from urllib import request
 import key
 import cv2
 import pyzbar.pyzbar as pyzbar
@@ -53,14 +51,19 @@ def post():
     )
     
 print("アイカツQRコードSlack送信システム")
-print('該当の画像があるパスまたはURLを入れてください')
+print('該当の画像があるパスを入れてください')
 print("QRを読み取る場合はQR-Readと入れてください")
 print("カメラが起動します")
 print("終了する場合はexitまたはCtrl+Dでお願いします")
+print("URLのサポートは打ち切りました。")
 
 while True:
-    path = input()
-    
+    try:
+        path = input()
+    except UnicodeDecodeError:
+        print("ファジングしようとするなあ！！！！！！！！！！！！！！！！")
+        sys.exit()
+        
     path = path.strip()
 
     if "exit" in path:
@@ -73,7 +76,11 @@ while True:
                     if "QR" not in path:
                             print("これ画像じゃないですよね...")
                             print("再実行しますか？[Y/N]")
-                            retry = input()
+                            try:
+                                retry = input()
+                            except EOFError:
+                                print("不正な文字列を入れてませんか？？？")
+                                sys.exit()
                             if 'Y' in retry or 'Yes' in retry or 'yes' in retry or 'y' in retry or 'YES' in retry:
                                 continue
                             elif 'N' in retry or 'No' in retry or 'no' in retry or 'n' in retry or 'NO' in retry:
@@ -102,60 +109,42 @@ while True:
                 qr_result = pyzbar.decode(flame)
                 if qr_result != []:
                     print(qr_result[0][0])
-                    if "http://aikatsu.com/qr/id=" in qr_result:
-                        print("旧カツのカードは対応していません。別のカードを読み込んでください。")
-                        print("該当の画像を入れてください")
-                        print("終了する場合はexitまたはCtrl+Dでお願いします")
-                        path = "none"
-                        continue
                     path = qr_result[0][0].decode('utf-8', 'ignore')
                     print(path)
+                    if "http://dcd.sc/" not in path and "http://aikatsu.com/qr/id=" in path and "AK" in path:
+                        print("アイカツ以外のカードは読み込めません。悪しからず。")
+                        sys.exit()
                     break
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+    
+    if "http://aikatsu.com/qr/id=" in path:
+        print("旧カツのカードは対応していません。別のカードを読み込んでください。")
+        print("該当の画像を入れてください")
+        print("終了する場合はexitまたはCtrl+Dでお願いします")
+        path = "none"
+        continue
+    
+    if "AK" in path:
+        print("旧カツのカードは対応していません。別のカードを読み込んでください。")
+        print("該当の画像を入れてください")
+        print("終了する場合はexitまたはCtrl+Dでお願いします")
+        path = "none"
+        continue
         
     cv2.destroyAllWindows()
 
-    if "com" in path or "jp" in path and "jpg" not in path and "jpeg" not in path:
-        print("画像ダウンロード開始...")
-        try:
-            URL = path
-        except ValueError:
-            print("不正なURLでは？？？")
-            print("再実行しますか？[Y/N]")
-
-            retry = input()
-            if 'Y' in retry or 'Yes' in retry or 'yes' in retry or 'y' in retry or 'YES' in retry:
-                    continue
-            elif 'N' in retry or 'No' in retry or 'no' in retry or 'n' in retry or 'NO' in retry:
-                    break
-            else:
-                print("リトライしてください")
-                continue
-        try:
-            request.urlretrieve(URL, "python.jpg")
-        except ValueError:
-            print("不正なURLでは？？？")
-            print("再実行しますか？[Y/N]")
-
-            retry = input()
-            if 'Y' in retry or 'Yes' in retry or 'yes' in retry or 'y' in retry or 'YES' in retry:
-                    continue
-            elif 'N' in retry or 'No' in retry or 'no' in retry or 'n' in retry or 'NO' in retry:
-                    break
-            else:
-                print("リトライしてください")
-                contunue
-        path = "python.jpg"
-        print("画像ダウンロード終了...")
-    
     if "http://dcd.sc/" not in path:
         try:
             read = decode(Image.open(path))
         except FileNotFoundError:
             print("本当にそこにありますか？？？")
             print("再実行しますか？[Y/N]")
-            retry = input()
+            try:
+                retry = input()
+            except EOFError:
+                print("不正な文字列を入れてませんか？？？")
+                sys.exit()
             if 'Y' in retry or 'Yes' in retry or 'yes' in retry or 'y' in retry or 'YES' in retry:
                 continue
             elif 'N' in retry or 'No' in retry or 'no' in retry or 'n' in retry or 'NO' in retry:
@@ -163,6 +152,11 @@ while True:
             else:
                 print("リトライしてください")
                 continue
+        except OSError:
+            print("画像ファイルに見せかけた不正なファイルを読み込ませないでください")
+            print("強制終了します")
+            sys.exit()
+            
         path = read[0][0].decode('utf-8', 'ignore')
     
     print(path)
